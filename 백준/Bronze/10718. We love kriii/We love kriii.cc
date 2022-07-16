5210299ecf4cd8fe8d8468bc3e287cb66dfabad6
Yejin Kwon <96688810+kwonyj1022@users.noreply.github.com>
2022-07-16 18:39:53 +0900

    [Bronze V] Title: We love kriii, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10718. We love kriii/We love kriii.cc
+++ 백준/Bronze/10718. We love kriii/We love kriii.cc
@@ -1,8 +1,7 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
-int main()
+int main(void)
 {
-	cout << "강한친구 대한육군" << endl << "강한친구 대한육군" << endl;
+	printf("강한친구 대한육군\n강한친구 대한육군");
 	return 0;
 }
