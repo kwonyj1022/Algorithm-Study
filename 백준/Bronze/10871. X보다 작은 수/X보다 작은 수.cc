0d755b86ff481e3ec82adffa556acc0ba4719204
Yejin Kwon <96688810+kwonyj1022@users.noreply.github.com>
2022-07-20 23:57:23 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	int N, X;
+	scanf("%d %d", &N, &X);
+	int* A = new int[N];
+	for (int i = 0; i < N; i++)
+		scanf("%d", &A[i]);
+
+	for (int i = 0; i < N; i++)
+		if (A[i] < X)
+			printf("%d ", A[i]);
+
+	return 0;
+}
