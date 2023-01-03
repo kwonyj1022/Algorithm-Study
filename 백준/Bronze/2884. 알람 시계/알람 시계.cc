cb025da56b94bc2ce0e629624848a547215e2eb6
Yejin Kwon <96688810+kwonyj1022@users.noreply.github.com>
2023-01-03 17:31:39 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+	int H, M;
+	scanf("%d %d", &H, &M);
+
+	M = M - 45;
+
+	if (M < 0) {
+		H = H - 1;
+		M = M + 60;
+	}
+	if (H < 0) {
+		H = H + 24;
+	}
+	
+	printf("%d %d", H, M);
+}
