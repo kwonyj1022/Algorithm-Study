c58c5c91f4ba5b928f2457c791ac8e1a84af1eae
Yejin Kwon <96688810+kwonyj1022@users.noreply.github.com>
2023-06-18 13:57:57 +0900

    [Silver V] Title: 색종이, Time: 120 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -7,28 +7,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[][] arr = new int[100][100];
-        for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 100; j++) {
-                arr[i][j] = 0;
-            }
-        }
         int n = Integer.parseInt(br.readLine());
-        for (int i = 0; i < n; i++) {
+        int sum = 0;
+        while (n-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
             for (int j = x; j < x + 10; j++) {
                 for (int k = y; k < y + 10; k++) {
-                    arr[j][k] = 1;
-                }
-            }
-        }
-
-        int sum = 0;
-        for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 100; j++) {
-                if (arr[i][j] == 1) {
-                    sum++;
+                    if (arr[j][k]++ == 0) {
+                        sum++;
+                    }
                 }
             }
         }
