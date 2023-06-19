8f223bcdac8ee7ca403b221f00ef0dbb2d157e1d
Yejin Kwon <96688810+kwonyj1022@users.noreply.github.com>
2023-06-19 23:57:29 +0900

    [Bronze II] Title: 단어의 개수, Time: 292 ms, Memory: 25088 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        List<String> words = new ArrayList<>();
+        while(st.hasMoreTokens()) {
+            words.add(st.nextToken());
+        }
+        System.out.println(words.size());
+    }
+}
