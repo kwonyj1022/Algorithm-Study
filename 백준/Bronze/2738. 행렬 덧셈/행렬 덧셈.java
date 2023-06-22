5f5d62707359654835986fdb3a4e84b8e024b54c
Yejin Kwon <96688810+kwonyj1022@users.noreply.github.com>
2023-06-23 00:01:48 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 172 ms, Memory: 15784 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -7,22 +7,24 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuffer sb = new StringBuffer();
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int[][] answer = new int[N][M];
+        int[][] A = new int[N][M];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                answer[i][j] = Integer.parseInt(st.nextToken());
+                A[i][j] = Integer.parseInt(st.nextToken());
             }
         }
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                System.out.print(answer[i][j] + Integer.parseInt(st.nextToken()) + " ");
+                sb.append(A[i][j] + Integer.parseInt(st.nextToken())).append(" ");
             }
-            System.out.println();
+            sb.append("\n");
         }
+        System.out.println(sb);
     }
 }
