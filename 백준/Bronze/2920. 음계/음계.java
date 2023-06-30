7d6e9f1a63e9f03bda5eb3cc6e27cab32e9e15dd
Yejin Kwon <96688810+kwonyj1022@users.noreply.github.com>
2023-06-30 23:33:28 +0900

    [Bronze II] Title: 음계, Time: 204 ms, Memory: 17600 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.java
+++ 백준/Bronze/2920. 음계/음계.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine().replace(" ", "");
-        if (s.equals("12345678")) {
+        String s = sc.nextLine();
+        if (s.equals("1 2 3 4 5 6 7 8")) {
             System.out.println("ascending");
-        } else if (s.equals("87654321")) {
+        } else if (s.equals("8 7 6 5 4 3 2 1")) {
             System.out.println("descending");
         } else {
             System.out.println("mixed");
