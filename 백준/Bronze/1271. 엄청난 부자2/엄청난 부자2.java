d12927825be580f61ed767610727b51d8419f667
Yejin Kwon <96688810+kwonyj1022@users.noreply.github.com>
2023-07-04 23:27:57 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 220 ms, Memory: 17812 KB -BaekjoonHub

--- 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        BigInteger n = in.nextBigInteger();
-        BigInteger m = in.nextBigInteger();
+        Scanner sc = new Scanner(System.in);
+        BigInteger n = sc.nextBigInteger();
+        BigInteger m = sc.nextBigInteger();
+        sc.close();
         System.out.println(n.divide(m));
         System.out.println(n.remainder(m));
     }
