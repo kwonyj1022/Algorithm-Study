2aa258200c6c29710de1235ef0417bd5221c0d5f
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2023-08-21 00:07:44 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 128 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        int d = A - B;
+        int h = V - A;
+        if (h % d == 0) {
+            System.out.println(h / d + 1);
+        } else {
+            System.out.println(h / d + 2);
+        }
+    }
+}
