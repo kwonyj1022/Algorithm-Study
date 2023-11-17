6fd0477a95dbb90f76ab588d492ed9c9653d3b17
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2023-11-17 12:10:07 +0900

    [Silver I] Title: 줄어들지 않아, Time: 116 ms, Memory: 13944 KB -BaekjoonHub

--- 백준/Silver/2688. 줄어들지 않아/줄어들지 않아.java
+++ 백준/Silver/2688. 줄어들지 않아/줄어들지 않아.java
@@ -8,24 +8,20 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
 
-        int maxN = 0;
         int[] input = new int[T];
         for (int i = 0; i < T; i++) {
             input[i] = Integer.parseInt(br.readLine());
-            if (input[i] > maxN) {
-                maxN = input[i];
-            }
         }
 
-        long[][] arr = new long[maxN][10];
-        for (int i = 0; i < maxN; i++) {
+        long[][] arr = new long[64][10];
+        for (int i = 0; i < 64; i++) {
             arr[i][0] = 1;
         }
         for (int i = 0; i < 10; i++) {
             arr[0][i] = i + 1;
         }
 
-        for (int i = 1; i < maxN; i++) {
+        for (int i = 1; i < 64; i++) {
             for (int j = 1; j < 10; j++) {
                 arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
             }
