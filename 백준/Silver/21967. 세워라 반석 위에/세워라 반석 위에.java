b4f1ac969d40a28ed44922ca29ff63b86cc2a1d2
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2023-11-21 11:51:04 +0900

    [Silver III] Title: 세워라 반석 위에, Time: 568 ms, Memory: 82216 KB -BaekjoonHub

--- 백준/Silver/21967. 세워라 반석 위에/세워라 반석 위에.java
+++ 백준/Silver/21967. 세워라 반석 위에/세워라 반석 위에.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,23 +9,33 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[] input = new int[N];
-        PriorityQueue<Integer> pqA = new PriorityQueue<>();
-        PriorityQueue<Integer> pqD = new PriorityQueue<>(Comparator.reverseOrder());
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[11];
+        int min = 0;
+        int max = 0;
         int s = 0;
-        int e = 0;
         int len = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             input[i] = Integer.parseInt(st.nextToken());
-            pqA.add(input[i]);
-            pqD.add(input[i]);
-            e = i;
-            if (pqD.peek() - pqA.peek() > 2) {
-                pqA.remove(input[s]);
-                pqD.remove(input[s]);
+            arr[input[i]]++;
+            for (int j = 1; j < 11; j++) {
+                if (arr[j] > 0) {
+                    min = j;
+                    break;
+                }
+            }
+            for (int j = 10; j > 0; j--) {
+                if (arr[j] > 0) {
+                    max = j;
+                    break;
+                }
+            }
+
+            if (max - min > 2) {
+                arr[input[s]]--;
                 s++;
             }
-            len = Math.max(len, e - s + 1);
+            len = Math.max(len, i - s + 1);
         }
         System.out.println(len);
     }
