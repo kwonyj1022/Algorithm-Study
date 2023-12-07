a0f20e1bc1aa709e8423e8ffac182f96f9927b63
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2023-12-08 00:02:56 +0900

    [Silver IV] Title: 소가 길을 건너간 이유 3, Time: 128 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Silver/14469. 소가 길을 건너간 이유 3/소가 길을 건너간 이유 3.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr, (o1, o2) -> o1[0] - o2[0]);
+
+        int time = 0;
+        for (int i = 0; i < N; i++) {
+            if (time < arr[i][0]) {
+                time = arr[i][0] + arr[i][1];
+            } else {
+                time += arr[i][1];
+            }
+        }
+
+        System.out.println(time);
+    }
+}
