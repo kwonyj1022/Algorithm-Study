a1b7461f5d55f5864c8261b7e1404203b7f09eee
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2023-12-13 22:31:56 +0900

    [Silver V] Title: 마트료시카 합치기, Time: 144 ms, Memory: 14644 KB -BaekjoonHub

---
+++ 백준/Silver/25631. 마트료시카 합치기/마트료시카 합치기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        int answer = 1;
+        int cnt = 1;
+        for (int i = 1; i < N; i++) {
+            if (arr[i - 1] != arr[i]) {
+                cnt = 1;
+            } else {
+                cnt++;
+                answer = Math.max(answer, cnt);
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
