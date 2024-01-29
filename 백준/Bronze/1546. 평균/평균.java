1143378b71a3d814331c9cc6398de810630a207b
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2024-01-29 19:25:08 +0900

    [Bronze I] Title: 평균, Time: 128 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -7,21 +7,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] score = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
         int M = 0;
+        int sum = 0;
         for (int i = 0; i < N; i++) {
             int num = Integer.parseInt(st.nextToken());
             if (num > M) {
                 M = num;
             }
-            score[i] = num;
+            sum += num;
         }
 
-        double sum = 0;
-        for (int i = 0; i < N; i++) {
-            sum += (double) score[i] / M * 100;
-        }
-        System.out.println(sum / N);
+        System.out.println(sum * 100.0 / M / N);
     }
 }
