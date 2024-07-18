4cb1e7cbe75fa48e2681ba178ef1b22728d73989
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2024-07-18 21:42:36 +0900

    [Gold V] Title: 토마토, Time: 648 ms, Memory: 100944 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -14,6 +14,7 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         boolean[][] arr = new boolean[N][M];
         Queue<Node> q = new ArrayDeque<>();
+        boolean[][] visited = new boolean[N][M];
         int remain = 0;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -22,6 +23,7 @@ public class Main {
                 if (t == 1) {
                     arr[i][j] = true;
                     q.offer(new Node(i, j, 0));
+                    visited[i][j] = true;
                     remain++;
                 } else if (t == 0) {
                     arr[i][j] = true;
@@ -31,14 +33,9 @@ public class Main {
         }
         int[] dr = {0, 0, 1, -1};
         int[] dc = {1, -1, 0, 0};
-        boolean[][] visited = new boolean[N][M];
         int maxday = 0;
         while (!q.isEmpty()) {
             Node poll = q.poll();
-            if (visited[poll.r][poll.c]) {
-                continue;
-            }
-            visited[poll.r][poll.c] = true;
             remain--;
             maxday = Math.max(maxday, poll.day);
             for (int i = 0; i < 4; i++) {
@@ -48,6 +45,7 @@ public class Main {
                     continue;
                 }
                 if (arr[nr][nc] && !visited[nr][nc]) {
+                    visited[nr][nc] = true;
                     q.offer(new Node(nr, nc, poll.day + 1));
                 }
             }
