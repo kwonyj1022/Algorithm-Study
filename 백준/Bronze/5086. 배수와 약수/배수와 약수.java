b324cd45aad1be0d3abb8dd7d7d61ed00424dfe7
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2024-07-20 23:42:25 +0900

    [Bronze III] Title: 배수와 약수, Time: 116 ms, Memory: 13948 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.java
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -1,15 +1,12 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         while (true) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
@@ -18,15 +15,14 @@ public class Main {
                 break;
             }
             if (a % b == 0) {
-                bw.write("multiple\n");
+                sb.append("multiple\n");
             } else if (b % a == 0) {
-                bw.write("factor\n");
+                sb.append("factor\n");
             } else {
-                bw.write("neither\n");
+                sb.append("neither\n");
             }
         }
-        bw.flush();
-        bw.close();
+        System.out.print(sb);
         br.close();
     }
 }
