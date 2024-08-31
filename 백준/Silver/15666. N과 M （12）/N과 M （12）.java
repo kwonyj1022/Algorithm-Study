2eb1d42a66aabb59ea33cd6abc8a1b14bbf046e7
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2024-09-01 01:12:03 +0900

    [Silver II] Title: N과 M (12), Time: 112 ms, Memory: 14456 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static int M;
+    static int[] arr;
+    static boolean[] check;
+    static int[] result;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        check = new boolean[N];
+        result = new int[M];
+        sb = new StringBuilder();
+        solve(0, 0);
+        System.out.print(sb);
+    }
+
+    static void solve(int preIdx, int cnt) {
+        if (cnt == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        int before = 0;
+        for (int i = preIdx; i < N; i++) {
+            if (before == arr[i]) {
+                continue;
+            }
+            check[i] = true;
+            before = arr[i];
+            result[cnt] = arr[i];
+            solve(i, cnt + 1);
+            check[i] = false;
+        }
+    }
+}
