ef2c30477b915c9f7e3cd77e5b2612683059a911
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2024-09-03 00:08:04 +0900

    [Silver I] Title: 정수 삼각형, Time: 240 ms, Memory: 26404 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -6,43 +6,33 @@ import java.util.StringTokenizer;
 public class Main {
 
     static int n;
-    static int[] arr;
+    static int[][] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        int size = n * (n + 1) / 2;
-        arr = new int[size];
+        arr = new int[n][n];
         int t = 0;
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            while (st.hasMoreTokens()) {
-                arr[t++] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j <= i; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
         int max = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
-                int target = i * (i + 1) / 2 + j;
-                arr[target] = Math.max(get(i - 1, j - 1), get(i - 1, j)) + arr[target];
-                max = Math.max(max, arr[target]);
+                arr[i][j] = Math.max(get(i - 1, j - 1), get(i - 1, j)) + arr[i][j];
+                max = Math.max(max, arr[i][j]);
             }
         }
         System.out.print(max);
     }
 
     static int get(int row, int idx) {
-        if (row < 0) {
+        if (row < 0 || idx < 0 || idx > row) {
             return 0;
         }
-        if (idx < 0) {
-            return 0;
-        }
-        int range = (row + 1) * (row + 2) / 2 - 1;
-        int target = row * (row + 1) / 2 + idx;
-        if (target > range) {
-            return 0;
-        }
-        return arr[target];
+        return arr[row][idx];
     }
 }
