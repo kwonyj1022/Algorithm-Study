3f8f681f688bc66344c4d0c92393c096ff28a5fd
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2024-09-17 01:27:40 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 124 ms, Memory: 17824 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,73 +1,58 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int N;
     static int K;
-    static List<Node>[] map;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        map = new ArrayList[100001];
-        for (int i = 0; i < 100001; i++) {
-            map[i] = new ArrayList<>();
-            if (i < 100000) {
-                map[i].add(new Node(i + 1, 1));
-            }
-            if (i > 0) {
-                map[i].add(new Node(i - 1, 1));
-            }
-            if (i <= 50000) {
-                map[i].add(new Node(i * 2, 0));
-            }
-        }
-
-        System.out.print(dijkstra(N, K));
+        System.out.print(bfs(N, K));
     }
 
-    static int dijkstra(int from, int to) {
+    static int bfs(int from, int to) {
+        Deque<Node> dq = new ArrayDeque<>();
         boolean[] visited = new boolean[100001];
-        int[] cost = new int[100001];
-        for (int i = 0; i < 100001; i++) {
-            cost[i] = Integer.MAX_VALUE;
-        }
-        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> o1.w - o2.w);
+        dq.addLast(new Node(from, 0));
+        visited[from] = true;
+        while (!dq.isEmpty()) {
+            Node node = dq.pollFirst();
+            if (node.x == K) {
+                return node.sec;
+            }
+            visited[node.x] = true;
+            int forward = node.x + 1;
+            int backward = node.x - 1;
+            int teleport = node.x * 2;
 
-        pq.offer(new Node(from, 0));
-        cost[from] = 0;
-        while (!pq.isEmpty()) {
-            Node current = pq.poll();
-            if (visited[current.v]) {
-                continue;
+            if (0 <= forward && forward < 100001 && !visited[forward]) {
+                dq.addLast(new Node(forward, node.sec + 1));
             }
-            visited[current.v] = true;
-            for (Node next : map[current.v]) {
-                int tmp = cost[current.v] + next.w;
-                if (tmp < cost[next.v]) {
-                    cost[next.v] = tmp;
-                    pq.offer(new Node(next.v, tmp));
-                }
+            if (0 <= backward && backward < 100001 && !visited[backward]) {
+                dq.addLast(new Node(backward, node.sec + 1));
+            }
+            if (0 <= teleport && teleport < 100001 && !visited[teleport]) {
+                dq.addFirst(new Node(teleport, node.sec));
             }
         }
-        return cost[to];
+        return -1;
     }
 
     static class Node {
-        int v;
-        int w;
+        int x;
+        int sec;
 
-        public Node(int v, int w) {
-            this.v = v;
-            this.w = w;
+        public Node(int x, int sec) {
+            this.x = x;
+            this.sec = sec;
         }
     }
 }
