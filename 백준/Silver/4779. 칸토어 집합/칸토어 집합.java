f304950726753c0c4ea5626f3f6a0c89fb9a8cb1
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2024-10-04 01:33:58 +0900

    [Silver III] Title: 칸토어 집합, Time: 164 ms, Memory: 23744 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -4,37 +4,29 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    static StringBuilder sb;
-
     public static void main(String[] args) throws IOException {
+        StringBuilder on = new StringBuilder("-");
+        StringBuilder off = new StringBuilder(" ");
+        String[] cache = new String[13];
+        cache[0] = on.toString();
+        for (int i = 1; i < 13; i++) {
+            String prevSpace = off.toString();
+            on.append(prevSpace).append(cache[i - 1]);
+            off.append(prevSpace).append(prevSpace);
+            cache[i] = on.toString();
+        }
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         while (true) {
             String input = br.readLine();
             if (input == null) {
                 break;
             }
-            long N = Long.parseLong(input);
-            solve((long) Math.pow(3, N), true);
-            sb.append('\n');
+            int N = Integer.parseInt(input);
+            sb.append(cache[N]).append('\n');
         }
-        System.out.print(sb);
-    }
 
-    static void solve(long n, boolean active) {
-        if (n == 1) {
-            sb.append(active ? '-' : ' ');
-            return;
-        }
-        long d = n / 3;
-        if (active) {
-            solve(d, true);
-            solve(d, false);
-            solve(d, true);
-        } else {
-            solve(d, false);
-            solve(d, false);
-            solve(d, false);
-        }
+        System.out.print(sb);
     }
 }
