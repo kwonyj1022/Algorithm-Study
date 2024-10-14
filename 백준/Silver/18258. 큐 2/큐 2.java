26c73ffbcd389b7f59a26f59d2113ee396bd6eaf
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2024-10-15 03:04:05 +0900

    [Silver IV] Title: 큐 2, Time: 1196 ms, Memory: 330328 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        Deque<Integer> dq = new ArrayDeque<>();
+        while (N-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String cmd = st.nextToken();
+            if (cmd.charAt(1) == 'u') {
+                dq.add(Integer.parseInt(st.nextToken()));
+            } else if (cmd.charAt(1) == 'o') {
+                sb.append(dq.isEmpty() ? -1 : dq.removeFirst()).append('\n');
+            } else if (cmd.charAt(1) == 'i') {
+                sb.append(dq.size()).append('\n');
+            } else if (cmd.charAt(1) == 'm') {
+                sb.append(dq.isEmpty() ? 1 : 0).append('\n');
+            } else if (cmd.charAt(1) == 'r') {
+                sb.append(dq.isEmpty() ? -1 : dq.peekFirst()).append('\n');
+            } else if (cmd.charAt(1) == 'a') {
+                sb.append(dq.isEmpty() ? -1 : dq.peekLast()).append('\n');
+            }
+        }
+        System.out.print(sb);
+    }
+
+    static class Sticker {
+        int r;
+        int c;
+        int score;
+
+        public Sticker(int r, int c, int score) {
+            this.r = r;
+            this.c = c;
+            this.score = score;
+        }
+    }
+}
