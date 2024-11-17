8be817c2cbe6ee767a2be940ed91e59ff1cef3e7
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2024-11-17 22:45:38 +0900

    [Silver I] Title: 그림, Time: 328 ms, Memory: 51368 KB -BaekjoonHub

---
+++ 백준/Silver/1926. 그림/그림.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n;
+    static int m;
+    static int[][] map;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int cnt = 0;
+        int maxSize = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (map[i][j] == 1) {
+                    maxSize = Math.max(maxSize, dfs(i, j));
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+        System.out.println(maxSize);
+    }
+
+    static int dfs(int r, int c) {
+        map[r][c] = 2;
+        int size = 1;
+        for (int i = 0; i < 4; i++) {
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+            if (nr >= 0 && nr < n && nc >= 0 && nc < m && map[nr][nc] == 1) {
+                size += dfs(nr, nc);
+            }
+        }
+        return size;
+    }
+}
