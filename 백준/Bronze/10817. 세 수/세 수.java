c49249f97137fb4d90c24c35d2bb5500e10eb5c0
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2024-12-24 22:15:27 +0900

    [Bronze III] Title: 세 수, Time: 104 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int[] arr = new int[3];
+        arr[0] = Integer.parseInt(input[0]);
+        arr[1] = Integer.parseInt(input[1]);
+        arr[2] = Integer.parseInt(input[2]);
+        Arrays.sort(arr);
+        System.out.print(arr[1]);
+    }
+}
