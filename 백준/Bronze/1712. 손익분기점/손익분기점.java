67e633637c182c2baa02e7f301f9b5329bda7d8a
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2025-01-03 22:09:22 +0900

    [Bronze II] Title: 손익분기점, Time: 116 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int A = Integer.parseInt(input[0]);
+        int B = Integer.parseInt(input[1]);
+        int C = Integer.parseInt(input[2]);
+        if (C <= B) {
+            System.out.print(-1);
+        } else {
+            System.out.print(A / (C - B) + 1);
+        }
+    }
+}
