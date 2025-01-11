7540e7ec2e141b6c10ab13ca22d546cca9720dad
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2025-01-11 21:05:15 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 364 ms, Memory: 50312 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
@@ -1,12 +1,10 @@
-import org.w3c.dom.Node;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.PriorityQueue;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -26,23 +24,33 @@ public class Main {
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
-            map[from].add(new Node(to, cost, new ArrayList<>()));
+            map[from].add(new Node(to, cost));
         }
         StringTokenizer st = new StringTokenizer(br.readLine());
         int start = Integer.parseInt(st.nextToken());
         int end = Integer.parseInt(st.nextToken());
+        int[] previous = new int[n + 1];
 
         boolean[] visited = new boolean[n + 1];
         PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> o1.cost - o2.cost);
-        pq.offer(new Node(start, 0, new ArrayList<>()));
+        pq.offer(new Node(start, 0));
         minCost[start] = 0;
         while (!pq.isEmpty()) {
             Node current = pq.poll();
             if (current.n == end) {
+                Stack<Integer> path = new Stack<>();
+                int last = end;
+                while (last != start) {
+                    path.add(last);
+                    last = previous[last];
+                }
+                path.add(start);
+
                 StringBuilder sb = new StringBuilder();
-                sb.append(current.cost).append("\n").append(current.path.size()).append("\n");
-                for (int pathNode : current.path) {
-                    sb.append(pathNode).append(" ");
+                sb.append(current.cost).append("\n");
+                sb.append(path.size()).append("\n");
+                while (!path.isEmpty()) {
+                    sb.append(path.pop()).append(" ");
                 }
                 System.out.print(sb);
                 return;
@@ -55,8 +63,9 @@ public class Main {
                 int nextCost = minCost[current.n] + next.cost;
                 if (minCost[next.n] > nextCost) {
                     minCost[next.n] = nextCost;
-                    Node newNode = new Node(next.n, nextCost, current.path);
+                    Node newNode = new Node(next.n, nextCost);
                     pq.offer(newNode);
+                    previous[next.n] = current.n;
                 }
             }
         }
@@ -65,13 +74,10 @@ public class Main {
     static class Node {
         int n;
         int cost;
-        List<Integer> path;
 
-        public Node(int n, int cost, List<Integer> prePath) {
+        public Node(int n, int cost) {
             this.n = n;
             this.cost = cost;
-            this.path = new ArrayList(prePath);
-            this.path.add(n);
         }
     }
 }
