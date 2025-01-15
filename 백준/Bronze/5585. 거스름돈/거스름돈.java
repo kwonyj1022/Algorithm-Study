1f1214ed9c322c5a435b763d32f22f45eccc84c9
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2025-01-15 14:54:45 +0900

    [Bronze II] Title: 거스름돈, Time: 104 ms, Memory: 14380 KB -BaekjoonHub

---
+++ 백준/Bronze/5585. 거스름돈/거스름돈.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = 1000 - Integer.parseInt(br.readLine());
+        int[] coin = {500, 100, 50, 10, 5, 1};
+        int answer = 0;
+        for (int i = 0; i < 6; i++) {
+            answer += n / coin[i];
+            n = n % coin[i];
+        }
+        System.out.print(answer);
+    }
+}
