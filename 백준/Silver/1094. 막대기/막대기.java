9471d40e3b05e822db5757d8a36ba2c649566cc4
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2025-01-17 23:52:24 +0900

    [Silver V] Title: 막대기, Time: 100 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.java
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -1,30 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int X = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o1 - o2);
-        pq.offer(64);
-        int sum = 64;
-        while (true) {
-            if (sum == X) {
-                break;
-            }
-            int min = pq.poll();
-            int half = min / 2;
-            if (sum - half >= X) {
-                pq.offer(half);
-                sum -= half;
-            } else {
-                pq.offer(half);
-                pq.offer(half);
+        char[] binary = Integer.toBinaryString(X).toCharArray();
+        int answer = 0;
+        for (int i = 0; i < binary.length; i++) {
+            if (binary[i] == '1') {
+                answer++;
             }
         }
-        System.out.print(pq.size());
+        System.out.print(answer);
     }
 }
