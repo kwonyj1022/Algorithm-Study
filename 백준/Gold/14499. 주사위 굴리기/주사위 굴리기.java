479d0d86d468223a8206df2ee2499d81ba07033c
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2025-03-05 00:12:46 +0900

    [Gold IV] Title: 주사위 굴리기, Time: 108 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.java
+++ 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.java
@@ -1,89 +1,79 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
+
     static int N, M, x, y;
     static int[][] map;
     static int[] dice;
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {1, -1, 0, 0};
-
+    static int topIdx, frontIdx, rightIdx;
+    static int[] dx, dy;
     static StringBuilder sb;
 
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        sb = new StringBuilder();
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        x = Integer.parseInt(st.nextToken());
-        y = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-
+        String[] input = br.readLine().split(" ");
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+        x = Integer.parseInt(input[2]);
+        y = Integer.parseInt(input[3]);
+        int K = Integer.parseInt(input[4]);
         map = new int[N][M];
         dice = new int[6];
-
-        for(int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < M; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+        topIdx = 0;
+        frontIdx = 1;
+        rightIdx = 2;
+        dx = new int[]{0, 0, 0, -1, 1};
+        dy = new int[]{0, 1, -1, 0, 0};
+        sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            input = br.readLine().split(" ");
+            for (int j = 0; j < M; j++) {
+                map[i][j] = input[j].charAt(0) - '0';
             }
         }
-
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < k; i++) {
-            move(Integer.parseInt(st.nextToken()) - 1);
+        input = br.readLine().split(" ");
+        for (int i = 0; i < K; i++) {
+            move(input[i].charAt(0) - '0');
         }
-
-        System.out.println(sb);
+        System.out.print(sb);
     }
 
     static void move(int cmd) {
         int nx = x + dx[cmd];
         int ny = y + dy[cmd];
-
-        if(nx < 0 || ny < 0 || nx >= N || ny >= M) {
+        if (nx < 0 || nx >= N || ny < 0 || ny >= M) {
             return;
         }
-
-        int tmp = dice[5];
-        switch (cmd) {
-            case 0:
-                dice[5] = dice[2];
-                dice[2] = dice[0];
-                dice[0] = dice[3];
-                dice[3] = tmp;
-                break;
-            case 1:
-                dice[5] = dice[3];
-                dice[3] = dice[0];
-                dice[0] = dice[2];
-                dice[2] = tmp;
-                break;
-            case 2:
-                dice[5] = dice[4];
-                dice[4] = dice[0];
-                dice[0] = dice[1];
-                dice[1] = tmp;
-                break;
-            default:
-                dice[5] = dice[1];
-                dice[1] = dice[0];
-                dice[0] = dice[4];
-                dice[4] = tmp;
-        }
-
-        sb.append(dice[0]).append("\n");
-
         x = nx;
         y = ny;
-        if(map[x][y] == 0) {
-            map[x][y] = dice[5];
+
+        if (cmd == 1) {
+            int tmp = topIdx;
+            topIdx = 5 - rightIdx;
+            rightIdx = tmp;
+        } else if (cmd == 2) {
+            int tmp = topIdx;
+            topIdx = rightIdx;
+            rightIdx = 5 - tmp;
+        } else if (cmd == 3) {
+            int tmp = topIdx;
+            topIdx = 5 - frontIdx;
+            frontIdx = tmp;
         } else {
-            dice[5] = map[x][y];
+            int tmp = topIdx;
+            topIdx = frontIdx;
+            frontIdx = 5 - tmp;
+        }
+
+        if (map[x][y] == 0) {
+            map[x][y] = dice[5 - topIdx];
+        } else {
+            dice[5 - topIdx] = map[x][y];
             map[x][y] = 0;
         }
+        sb.append(dice[topIdx]).append('\n');
     }
 }
