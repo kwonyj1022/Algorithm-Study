1d3371cd32e3b94ae957c03b66bcd0f65438292c
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2025-03-23 23:01:05 +0900

    [Silver IV] Title: 쿠키의 신체 측정, Time: 248 ms, Memory: 19676 KB -BaekjoonHub

---
+++ 백준/Silver/20125. 쿠키의 신체 측정/쿠키의 신체 측정.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] heart = new int[2];
+        int[] answer = new int[5];
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < N; j++) {
+                if (input.charAt(j) == '_') {
+                    continue;
+                }
+                if (heart[0] == 0) {
+                    heart[0] = i + 1;
+                    heart[1] = j;
+
+                    continue;
+                }
+                if (i == heart[0]) {
+                    if (j < heart[1]) {
+                        answer[0]++;
+                    } else if (j > heart[1]) {
+                        answer[1]++;
+                    }
+                    continue;
+                }
+                if (j == heart[1]) {
+                    answer[2]++;
+                    continue;
+                }
+                if (j < heart[1]) {
+                    answer[3]++;
+                } else {
+                    answer[4]++;
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(heart[0] + 1).append(" ").append(heart[1] + 1).append("\n");
+        for (int i = 0; i < 5; i++) {
+            sb.append(answer[i]).append(" ");
+        }
+        System.out.print(sb);
+    }
+}
