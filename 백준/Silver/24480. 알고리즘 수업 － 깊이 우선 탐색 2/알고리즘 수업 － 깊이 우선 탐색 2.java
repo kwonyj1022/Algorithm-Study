599952b3bf6256aa80a59dacddd4f040cc863dcb
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2025-05-04 23:53:12 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 928 ms, Memory: 86812 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    static int N, M, R;
+    static List<List<Integer>> graph;
+    static int[] visited;
+    static StringBuilder sb;
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        graph = new ArrayList<>(N + 1);
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+        for (int i = 1; i <= N; i++) {
+            graph.get(i).sort((o1, o2) -> o2 - o1);
+        }
+
+        sb = new StringBuilder();
+        visited = new int[N + 1];
+        n = 1;
+        dfs(R, 0);
+        for (int i = 1; i <= N; i++) {
+            sb.append(visited[i]).append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    static void dfs(int start, int depth) {
+        if (depth == N) {
+            return;
+        }
+        visited[start] = n++;
+        for (int next : graph.get(start)) {
+            if (visited[next] == 0) {
+                dfs(next, depth + 1);
+            }
+        }
+    }
+}
