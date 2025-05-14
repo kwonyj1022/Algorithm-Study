94b779d0890438e9822d4697c1a51c55c06a1941
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2025-05-14 20:04:48 +0900

    [Silver III] Title: 합 구하기, Time: 560 ms, Memory: 50252 KB -BaekjoonHub

---
+++ 백준/Silver/11441. 합 구하기/합 구하기.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int[] sum = new int[N + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            sum[i] = sum[i - 1] + Integer.parseInt(st.nextToken());
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+
+            sb.append(sum[j] - sum[i - 1]).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
